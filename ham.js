d2620d571efd6bc7fc303c528b0383f2b7da99ff
pointblank <40745901+pointblankcompany@users.noreply.github.com>
2019-08-19 11:26:57 -0700

    Updates

---
+++ ham.js
@@ -0,0 +1,19 @@
+function menu() {
+  document.getElementById('menu').style.cssText = 'top:0px;display:flex;flex-flow:column;height:500px;overflow:visible;justify-content:flex-start;';
+  document.getElementById('ham').style.cssText = 'display:none;';
+}
+
+function ham() {
+  document.getElementById('menu').style.cssText = 'display:none;';
+  document.getElementById('ham').style.cssText = 'display:block; margin:1rem;';
+}
+
+function show() {
+  document.getElementById('read').style.cssText = 'height:500px;overflow:hidden;transition:height 500ms ease;';
+  document.getElementById('show').style.cssText = 'display:none;';
+}
+
+function hide() {
+  document.getElementById('read').style.cssText = 'height:0px;overflow:hidden;transition:height 500ms ease;';
+  document.getElementById('show').style.cssText = 'display:block;';
+}
